1c631ba57370b6cee0670e83485706b1ea7c2306
Hitesh Thaliya <hiteshthaliya123@gmail.com>
2026-09-18 00:40:10 +0530

    add while loop

--- 16_square_1_to_n.py
+++ 16_square_1_to_n.py
@@ -4,7 +4,9 @@ n = int(input("Enter number: "))
 for i in range(1,n+1):
     square = i**2
     print(square,end=" ")
+
 #-----While loop--------
+
 a = int(input("\nEnter number: ")) 
 i =1
 while i<=n:

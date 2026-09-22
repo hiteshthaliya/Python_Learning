3ae2d81abb746a8e35ef4ba2fb8aadf84edc3be3
Hitesh Thaliya <hiteshthaliya123@gmail.com>
2026-09-22 16:48:47 +0530

    Implement Armstrong number checker and range printer
    Added functionality to check if a number is an Armstrong number and to print all Armstrong numbers between 1 and 1000.

--- For_Loop_Practice/19_armstrong_number.py
+++ For_Loop_Practice/19_armstrong_number.py
@@ -1,1 +1,25 @@
+###---------------- Armstrong number ---------#
+#An Armstrong number is a number where the sum of each digit raised to the power of the total number of digits equals the original number.
+n = int(input("enter number for checking:- "))
+original_n = n
+total_digit = len(str(n))
+total =0
+for digit in str(n):
+    total += int(digit)**total_digit
+if total==original_n:
+    print("Armstrong number")    
+else:
+    print("Not an Armstrong number")
 
+
+#--------armstron number between 1 to 1000-----------#
+for num in range(1,1001):
+    original = num
+    total_digit = len(str(num))
+    total = 0
+
+    for digit in str(num):
+        total +=int(digit)**total_digit
+
+    if total == original:
+        print(original)    

04c17d3987fd221d104c98865d175e2659c9f357
Hitesh Thaliya <hiteshthaliya123@gmail.com>
2026-09-23 22:05:14 +0530

    initial commit

---
+++ Pattern_printing_Practice/05_increasing_star_triangle.py
@@ -0,0 +1,12 @@
+#  print pattern in increasing order:- 
+# *
+# **
+# ***
+# ****
+# *****
+
+n = int(input("enter number: "))
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end=" ")
+    print()    
